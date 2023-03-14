f414e7a3bd145554766efb5a763af37ebe7c05ff
TubaComic <manumel1504@gmail.com>
2023-03-14 08:02:52 +0100

    Controller and Wlan view changes

--- lib/main.dart
+++ lib/main.dart
@@ -19,10 +19,6 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('TURO Controller'),
-        leading: Icon(Icons.menu),
-      ),
       backgroundColor: Colors.black26,
       body: Center(
         child: Column(
@@ -55,6 +51,120 @@ class MyHomePage extends StatelessWidget {
 class ConnectWlan extends StatefulWidget {
   @override
   _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<ConnectWlan> {
+
+  List<WifiNetwork> _wifiList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    getWifiList();
+  }
+
+  Future<void> getWifiList() async {
+    List<WifiNetwork> wifiList;
+    try {
+      wifiList = await WiFiForIoTPlugin.loadWifiList();
+    } on Exception catch (e) {
+      print(e.toString());
+      wifiList = [];
+    }
+
+    if (!mounted) return;
+
+    setState(() {
+      _wifiList = wifiList;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        alignment: const Alignment(0, 0,
+        ),
+        children: <Widget> [
+          Positioned(
+            top: 700.5,
+            left: 150.5,
+            width: 109,
+            height: 43,
+            child: ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.black),
+              ),
+              child: Text('Refresh'),
+              onPressed: () async {
+                getWifiList();
+              },
+            ),
+          ),
+          Expanded(
+            child: ListView.separated(
+              padding: EdgeInsets.only(top: 320),
+              separatorBuilder: (context, index) => Divider(),
+              itemCount: _wifiList.length,
+              itemBuilder: (context, index) => ListTile(
+                title: Text(_wifiList[index].ssid.toString()),
+                trailing:
+                Text('${_wifiList[index].level} ${_wifiList[index].frequency}'),
+                onTap: () async {
+                  // connect to wifi network
+                  bool result = await WiFiForIoTPlugin.connect(_wifiList[index].ssid.toString());
+                  print(result ? 'connected' : 'connection failed');
+                },
+              ),
+            ),
+          ),
+          Positioned(
+            top: 800.5,
+            left: 150.5,
+            width: 109,
+            height: 43,
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => Configure()));
+              },
+              child: Text(
+                'Connect',
+                style: TextStyle(
+                  shadows: [],
+                  color: const Color(
+                    0xFFFFFFFF,
+                  ),
+                  backgroundColor: const Color(
+                    0,
+                  ),
+                ),
+              ),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.black),
+              ),
+            ),
+          ),
+          Positioned(
+            top: 0,
+            left: 0,
+            width: 415,
+            height: 290,
+            child: Image(
+              image: const AssetImage(
+                'assets/ProteusBack.png',
+              ),
+              fit: BoxFit.cover,
+            ),
+          )
+        ],
+      ),
+      backgroundColor: const Color(
+        0xFF42BEA5,
+      ),
+    );
+  }
+
 }
 
 class Configure extends StatelessWidget {
@@ -162,33 +272,7 @@ class Configure extends StatelessWidget {
   }
 }
 
-
-class _MyAppState extends State<ConnectWlan> {
-
-  List<WifiNetwork> _wifiList = [];
-
-  @override
-  void initState() {
-    super.initState();
-    getWifiList();
-  }
-
-  Future<void> getWifiList() async {
-    List<WifiNetwork> wifiList;
-    try {
-      wifiList = await WiFiForIoTPlugin.loadWifiList();
-    } on Exception catch (e) {
-      print(e.toString());
-      wifiList = [];
-    }
-
-    if (!mounted) return;
-
-    setState(() {
-      _wifiList = wifiList;
-    });
-  }
-
+class Controller extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -196,29 +280,7 @@ class _MyAppState extends State<ConnectWlan> {
         fit: StackFit.expand,
         alignment: const Alignment(0, 0,
         ),
-        children: <Widget> [
-          ElevatedButton(
-            child: Text('Get List of Wifi'),
-            onPressed: () async {
-              getWifiList();
-            },
-          ),
-          Expanded(
-            child: ListView.separated(
-              separatorBuilder: (context, index) => Divider(),
-              itemCount: _wifiList.length,
-              itemBuilder: (context, index) => ListTile(
-                title: Text(_wifiList[index].ssid.toString()),
-                trailing:
-                Text('${_wifiList[index].level} ${_wifiList[index].frequency}'),
-                onTap: () async {
-                  // connect to wifi network
-                  bool result = await WiFiForIoTPlugin.connect(_wifiList[index].ssid.toString());
-                  print(result ? 'connected' : 'connection failed');
-                },
-              ),
-            ),
-          ),
+        children: [
           Positioned(
             top: 600.5,
             left: 150.5,
@@ -226,9 +288,13 @@ class _MyAppState extends State<ConnectWlan> {
             height: 43,
             child: ElevatedButton(
               onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => Configure()));
+                context;
+                    (context) {
+                  return ConnectWlan();
+                };
               },
-              child: Text(
+              child:
+              Text(
                 'Connect',
                 style: TextStyle(
                   shadows: [],
@@ -256,7 +322,52 @@ class _MyAppState extends State<ConnectWlan> {
               ),
               fit: BoxFit.cover,
             ),
-          )
+          ),
+          Positioned(
+            top: 300,
+            left: 30,
+            width: 350,
+            height: 200,
+            child: TextField(
+              controller : TextEditingController(),
+              onSubmitted : (value) {
+                print('The text is $value');
+              },
+              decoration: InputDecoration(
+                hintText: 'Street color', // The passive text to show
+              ),
+            ),
+          ),
+          Positioned(
+            top: 400,
+            left: 30,
+            width: 350,
+            height: 200,
+            child: TextField(
+              controller : TextEditingController(),
+              onSubmitted : (value) {
+                print('The text is $value');
+              },
+              decoration: InputDecoration(
+                hintText: 'Line color', // The passive text to show
+              ),
+            ),
+          ),
+          Positioned(
+            top: 500,
+            left: 30,
+            width: 350,
+            height: 200,
+            child: TextField(
+              controller : TextEditingController(),
+              onSubmitted : (value) {
+                print('The text is $value');
+              },
+              decoration: InputDecoration(
+                hintText: 'Bus stop line color', // The passive text to show
+              ),
+            ),
+          ),
         ],
       ),
       backgroundColor: const Color(
@@ -264,49 +375,4 @@ class _MyAppState extends State<ConnectWlan> {
       ),
     );
   }
-
 }
-
-
-
-
-/*
-@override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Wifi IoT plugin example app'),
-        ),
-        body: Center(
-          child: Column(
-            children: <Widget>[
-              ElevatedButton(
-                child: Text('Get List of Wifi'),
-                onPressed: () async {
-                  getWifiList();
-                },
-              ),
-              Expanded(
-                child: ListView.separated(
-                  separatorBuilder: (context, index) => Divider(),
-                  itemCount: _wifiList.length,
-                  itemBuilder: (context, index) => ListTile(
-                    title: Text(_wifiList[index].ssid.toString()),
-                    trailing:
-                    Text('${_wifiList[index].level} ${_wifiList[index].frequency}'),
-                    onTap: () async {
-                      // connect to wifi network
-                      bool result = await WiFiForIoTPlugin.connect(_wifiList[index].ssid.toString());
-                      print(result ? 'connected' : 'connection failed');
-                    },
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-* */
